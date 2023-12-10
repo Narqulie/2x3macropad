f431356e796fcf21d09e9e71a4f2fc92354e0003
narqulie <Narqulie@gmail.com>
2023-12-10 20:49:47 +0200

    first commit

---
+++ keymaps/via/keymap.c
@@ -0,0 +1,18 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* THIS IS OVERWRITTEN IN VIA 
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_macropad(
+        KC_A,    KC_B,    KC_C,
+        KC_D,    KC_E,    KC_F
+    )
+};
